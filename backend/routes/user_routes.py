e737d962f88fcecf216d7e4de9d42fda88217dc1
MikolajDro <115974783+MikolajDro@users.noreply.github.com>
2025-01-11 01:16:48 +0100

    Update user_routes.py
    Without backend.extentions insted of extentions (line:4) flask app does not work

--- backend/routes/user_routes.py
+++ backend/routes/user_routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify, request
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from backend.models import User
-from extensions import db
+from backend.extensions import db
 
 user_bp = Blueprint("user", __name__)
 
